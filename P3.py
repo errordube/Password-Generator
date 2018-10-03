5a6a3abfbbbdd140807433a494b9184bfac7447d
Aditya Dube <32643633+errordube@users.noreply.github.com>
2018-10-04 01:01:55 +0530

    Add files via upload
    Password Generator

---
+++ P3.py
@@ -0,0 +1,12 @@
+import *
+
+letters = string.ascii_letters 
+digits = string.digits 
+symbols = string.punctuation
+chars = letters + digits + symbols
+
+min_length = 8
+max_length = 16
+password = "".join(choice(chars) for x in range(randint(min_length, max_length)))
+print(password)import string
+from random
